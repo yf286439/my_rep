b09f41d3dab98c0b753c346ac24c5a966c9ba8be
yf286439 <yf19921128@163.com>
2022-09-02 11:28:01 +0800

    将暂存区内容添加到仓库中

---
+++ ctx/src/main/java/com/yangfei/ctx/CtxApplication.java
@@ -0,0 +1,13 @@
+package com.yangfei.ctx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtxApplication.class, args);
+	}
+
+}
